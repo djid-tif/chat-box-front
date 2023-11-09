9695b0b2738f3ca9e8e63b70cc17c388d710835d
Romain <r.huc@orange.fr>
2023-11-09 14:25:12 +0100

    featrure/ adding chatbox featurres

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {io} from "socket.io-client";
 
 function App() {
+  const [input, setInput] = useState('');
+  const [pseudo, setPseudo] = useState('');
+  const [color, setColor] = useState('#000000');
+   const socket = io('http://localhost:3001');
+
+
+  useEffect(() => {
+    // Listen for messages from the server
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    // Clean up the effect when the component unmounts
+    return () => socket.off('message');
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      const newMessage = { text: pseudo + ': ' + input, color: color };
+      socket.emit('message', newMessage); // Emit the message to the server
+      setInput(''); // Clear the input after sending
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>ChatBox</h1>
+        </header>
+        <div className="chat-box">
+          <div className="messages">
+            {messages.map((message, index) => (
+                <div key={index} className="message" style={{ color: message.color }}>
+                  {message.text}
+                </div>
+            ))}
+          </div>
+          <form onSubmit={sendMessage}>
+            <input
+                type="color"
+                value={color}
+                className="color-picker"
+                onChange={(e) => setColor(e.target.value)}
+                title="Choisir votre couleur"
+            />
+            <input
+                type="text"
+                value={pseudo}
+                onChange={(e) => setPseudo(e.target.value)}
+                placeholder="Pseudo"
+            />
+            <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Écrire un message..."
+            />
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
+      </div>
   );
 }
 
